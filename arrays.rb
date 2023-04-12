8e407750d7a6d4315e084c54cbb03582dfa65ed5
Hayden McSorley <haymcsorley@gmail.com>
2023-04-12 18:30:00 -0500

    Add arrays

---
+++ arrays.rb
@@ -0,0 +1,12 @@
+
+names = ["ben", "eli", "jon", "sue"]
+puts names
+
+ages = [26, 31, 19, 33]
+puts ages
+
+prices = [100.00, 210.99, 90.00, 28.75]
+puts prices
+
+statuses = [true, false, false, true]
+puts statuses
