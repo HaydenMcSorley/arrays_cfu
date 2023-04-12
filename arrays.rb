69b93779c010b7dcf600c8ae09bbfc02784bb01a
Hayden McSorley <haymcsorley@gmail.com>
2023-04-12 18:37:52 -0500

    Add methods to each of the arrays

--- arrays.rb
+++ arrays.rb
@@ -1,12 +1,23 @@
-
+# This will remove one item from the end of the array
 names = ["ben", "eli", "jon", "sue"]
+
+names.pop
 puts names
 
+# this will add 36 to the end of the array
 ages = [26, 31, 19, 33]
+
+ages.push(36)
 puts ages
 
+# this will remove one item from the front of the array
 prices = [100.00, 210.99, 90.00, 28.75]
+
+prices.shift
 puts prices
 
+# this will add false to the front of the array
 statuses = [true, false, false, true]
+
+statuses.unshift(false)
 puts statuses
